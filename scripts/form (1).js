ba6980338881e6088354d15aa12c05ff66b4ec3e
{{Yumasse}} <{{Y.Chekanyuk_FIT_12_22_B_d@knute.edu.ua}}>
2023-11-22 14:44:13 +0200

    second commit

--- scripts/form (1).js
+++ scripts/form (1).js
@@ -1,51 +1,75 @@
-const MODAL_ACTIVE_CLASS_NAME = 'modal-active';
+function performlist() {
+  window.location.href = `list.html`;
+}
 
-const formModal = document.querySelector('#form-modal');
-const successModal = document.querySelector('#success-modal');
-const form = document.querySelector('#form');
-
-const openFormModalBtn = document.querySelector('#open-form-modal-btn');
-const launchBtn = document.querySelector('#launch-btn');
-const closeBtns = document.querySelectorAll('.close-btn');
-
-openFormModalBtn.addEventListener('click', () => {
-    formModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
-})
-
-const closeFormModal = () => {
-    formModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
-};
-
-const closeSuccessModal = () => {
-    successModal.classList.remove(MODAL_ACTIVE_CLASS_NAME);
-};
-
-const openSuccessModal = () => {
-    successModal.classList.add(MODAL_ACTIVE_CLASS_NAME);
-};
-
-closeBtns.forEach(btn => {
-    btn.addEventListener('click', e => {
-        e.stopPropagation();
-        closeFormModal();
-        closeSuccessModal();
-    })
-})
+function performpost1() {
+  window.location.href = `post1.html`;
+}
 
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    const formData = new FormData(form);
 
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => {
-        closeFormModal();
-        setTimeout(openSuccessModal, 700);
-        setTimeout(closeSuccessModal, 3000);
-      })
-      .catch((error) => console.log('Sending form failed'));
-})
+function performpost2() {
+
+  window.location.href = `post2.html`;
+}
+
+
+
+function performbook1() {
+  window.location.href = `book1.html`;
+}
+
+function performbook2() {
+
+
+    window.location.href = 'book2.html';
+}
+
+function performback() {
+  window.location.href = `index.html`;
+}
+
+
+
+
+
+
+
+
+
+function scrollToAbout() {
+    var eventsSection = document.getElementById('about');
+
+    if (eventsSection) {
+        eventsSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+}
+
+function scrollToEvents() {
+    var eventsSection = document.getElementById('events');
+
+    if (eventsSection) {
+        eventsSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+}
+
+
+
+function scrollToContacts() {
+    var eventsSection = document.getElementById('contacts');
+
+    if (eventsSection) {
+        eventsSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+}
+
+
